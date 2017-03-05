be923a14731a07bbd02837fd45b4c8dc91b2dede
LE5DW2NCYGSCQUC\Administrator <1014398744@qq.com>
2017-03-05 16:51:29 +0800

    Add a HelloWorld class.

---
+++ src/main/java/com/expedia/common/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.expedia.common;
+
+/**
+ * Created by Administrator:Hunter on 2017/3/5.
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+}
